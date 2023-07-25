0c473d2b9808458140d557f58f249d818b102542
Len <lennemo09@gmail.com>
2023-07-25 07:17:35 +0000

    Updated shell script for GPU checks on slurm nodes.

--- check_node_smi.sh
+++ check_node_smi.sh
@@ -6,12 +6,16 @@ output_file="nvidia_smi_output.txt"
 > "$output_file"
 
 for i in {1..10}; do
+    timeout_duration=5
     # Run the srun command and store the output in a variable
-    srun_output=$(srun -w slurmnode${i} nvidia-smi 2>&1)
+    srun_output=$(timeout $timeout_duration srun -w slurmnode${i} nvidia-smi 2>&1)
 
     # Check if the srun output contains the message "queued and waiting for resources"
     if ! echo "$srun_output" | grep -q "queued and waiting for resources"; then
         echo -e "======================\nNode ${i}\n" >> "$output_file"
         echo "$srun_output" >> "$output_file"
+    else
+        echo -e "======================\nNode ${i}\n" >> "$output_file"
+        echo "Node ${i} is not available" >> "$output_file"
     fi
 done
